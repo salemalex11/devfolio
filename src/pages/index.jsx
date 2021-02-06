adf08debf793e33ec6d7b4983869bc50e441611e
Alex Lamouri <alexandre.gagne@mail.utoronto.ca>
2021-02-06 00:52:52 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,14 +24,12 @@ const Index = ({ data }) => {
       <SEO />
       <Header metadata={data.site.siteMetadata} />
       {about && <SectionAbout about={about} />}
+      {education && education.length && <SectionEducation education={education} />}
       {experience && experience.length && (
         <SectionExperience experience={experience} />
       )}
-      {education && education.length && <SectionEducation education={education} />}
+       {projects && projects.length && <SectionProjects projects={projects} />}
       {skills && skills.length && <SectionSkills skills={skills} />}
-      {projects && projects.length && <SectionProjects projects={projects} />}
-      
-      
     </Layout>
   );
 };
